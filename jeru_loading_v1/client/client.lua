41dd2c97bb4a4913bcb5a76d083cfcfc0c96bb4e
jerusalem <zikobedirhan98@gmail.com>
2025-02-01 00:14:27 +0300

    Add files via upload

---
+++ jeru_loading_v1/client/client.lua
@@ -0,0 +1,4 @@
+-- Boş client script dosyası
+-- Bu dosya FXServer için kullanılacaktır
+
+-- Buraya client-side kodlarınızı ekleyebilirsiniz
---
+++ jeru_loading_v1/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'jerusalem'
+
+lua54 'yes'
+
+files{
+    "index.html",
+    "app.js",
+    "particles.js",
+    "style.css",
+    "music/music.mp3",
+    "images/profile1.jpg",
+    "images/profile2.jpg",
+    "images/profile3.jpg",
+    "images/profile4.jpg",
+    "images/profile5.jpg",
+    "images/screenshot1.jpg",
+    "images/screenshot2.jpg",
+    "images/album-cover.jpg"
+}
+
+shared_script 'client/client.lua'
+
+loadscreen 'index.html'
